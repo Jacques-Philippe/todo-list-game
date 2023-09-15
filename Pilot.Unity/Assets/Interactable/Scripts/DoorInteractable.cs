c23e4bc60588961c390682997e3aaffc9e2ac33c
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-09-14 20:34:10 -0400

    Create DoorInteractable.cs

---
+++ Pilot.Unity/Assets/Interactable/Scripts/DoorInteractable.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DoorInteractable : BaseInteractable
+{
+    [SerializeField]
+    private Scenes scene;
+
+    public override void Interact()
+    {
+        SceneUtils.Load(scene);
+    }
+}

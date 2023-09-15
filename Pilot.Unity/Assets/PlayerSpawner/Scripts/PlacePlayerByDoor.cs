43d891f627e0a36f048cdd735e360ed5549d13da
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-09-15 08:53:02 -0400

    Add small comments

--- Pilot.Unity/Assets/PlayerSpawner/Scripts/PlacePlayerByDoor.cs
+++ Pilot.Unity/Assets/PlayerSpawner/Scripts/PlacePlayerByDoor.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+/// <summary>
+/// Class to place the player at a door given they're coming from another room
+/// </summary>
 public class PlacePlayerByDoor : MonoBehaviour
 {
     [Serializable]
     public struct SpawnPositions {
+        [Tooltip("The scene the player is coming from")]
         public Scenes lastScene;
+        [Tooltip("The place the player should be spawned given they're coming from lastScene")]
         public Transform spawnPosition;
     }
 
+    [Tooltip("The list of all spawn positions for the scene")]
     [SerializeField]
     private List<SpawnPositions> spawnPositions;
 

509e29cb70e5d649ce036accec9dc427112e128b
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2023-09-16 08:25:07 -0400

    Interactable close enough to player is detected

--- Pilot.Unity/Assets/Interactable/Scripts/InteractableProvider.cs
+++ Pilot.Unity/Assets/Interactable/Scripts/InteractableProvider.cs
@@ -6,10 +6,17 @@ public class InteractableProvider
 {
     public readonly string INTERACTABLE_TAG = "Interactable";
 
+    private float interactionDistance;
+
+    public InteractableProvider(float interactionDistance)
+    {
+        this.interactionDistance = interactionDistance;
+    }
+
     public IInteractable GetInteractable(Transform player)
     {
         var interactables = FindInteractables();
-        return Closest(interactables, player);
+        return ClosestInteractableNotTooFar(interactables, player);
     }
 
     private List<Transform> FindInteractables()
@@ -17,7 +24,7 @@ public class InteractableProvider
         return GameObject.FindGameObjectsWithTag(INTERACTABLE_TAG).Select(g => g.transform).ToList();
     }
 
-    private IInteractable Closest(List<Transform> transforms, Transform target)
+    private IInteractable ClosestInteractableNotTooFar(List<Transform> transforms, Transform target)
     {
         if (transforms.Count < 1)
         {
@@ -31,6 +38,11 @@ public class InteractableProvider
                 closest = i;
             }
         }
+        //if the interactable is too far from the player
+        if (IsInteractableTooFar(closest: closest, player: target))
+        {
+            return null;
+        }
 
         return closest.GetComponentInChildren<IInteractable>();
     }
@@ -41,4 +53,12 @@ public class InteractableProvider
         float bDist = (target - bPos).magnitude;
         return aDist < bDist;
     }
+
+    private bool IsInteractableTooFar(Transform closest, Transform player)
+    {
+        float distanceToPlayer = (closest.position - player.position).magnitude;
+        //if the interactable is too far from the player
+        return distanceToPlayer > interactionDistance;
+    }
+
 }
--- Pilot.Unity/Assets/Scripts/PlayerInteraction.cs
+++ Pilot.Unity/Assets/Scripts/PlayerInteraction.cs
@@ -5,16 +5,26 @@ public class PlayerInteraction : MonoBehaviour
 {
     public Action OnPlayerInteracted;
 
-    private InteractableProvider interactableProvider = new InteractableProvider();
-
     [SerializeField]
     private Transform player;
 
+    [SerializeField]
+    private float interactionDistance;
+
+    private InteractableProvider interactableProvider;
+
+    private void Awake()
+    {
+        this.interactableProvider = new InteractableProvider(interactionDistance: this.interactionDistance);
+    }
+
     void Update()
     {
+        var i = interactableProvider.GetInteractable(player);
+        Debug.Log($"Interactable {i}");
+
         if (Input.GetKeyDown(KeyCode.E))
         {
-            var i = interactableProvider.GetInteractable(player);
             i.Interact();
         }
     }
